e28a0d1a16332b1e91ca2173791a8c0be0057088
NeilTheRealDeal <neiltherealestdeal@gmail.com>
2026-05-09 15:34:34 +0800

    create: data science and data visualization programs and plots

---
+++ Scripts/datascience.R
@@ -0,0 +1,13 @@
+data(mtcars)
+
+# Create linear regression model
+model <- lm(mpg ~ wt, data = mtcars)
+
+# Show results
+summary(model)
+
+library(ggplot2)
+
+ggplot(mtcars, aes(x = wt, y = mpg)) +
+  geom_point() +
+  geom_smooth(method = "lm", color = "red")
---
+++ Scripts/ggplot.r
@@ -0,0 +1,9 @@
+library(ggplot2)
+
+data(mtcars)
+
+ggplot(mtcars, aes(x = wt, y = mpg)) +
+  geom_point(color = "blue") +
+  ggtitle("Car Weight vs MPG") +
+  xlab("Weight") +
+  ylab("Miles per Gallon")
